c2ddcb816ce1f2a2fdb6f0a858722ee15c7a097a
Marios MacBook Pro 15 <mfournarakis@gmail.com>
2018-09-27 15:02:40 +0200

    first commit

---
+++ MyModels.py
@@ -0,0 +1,106 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+
+
+class AutoEncoder(nn.Module):
+    """
+    Autoencoder module for intepretable transformations
+    """
+    def __init__(self,dropout_rate):
+        super(AutoEncoder, self).__init__()
+
+        #Encoder is a RenNet18
+        pretrained=models.resnet18(pretrained=True)
+    
+        pretrained.avgpool=nn.AdaptiveAvgPool2d(1) 
+
+        #Remove the last  fc layer
+
+        self.encoder= nn.Sequential(*list(pretrained.children())[:-1]) 
+
+        self.decoder=Decoder()
+
+        #Dropout on the compressed encoding
+        self.dropout=nn.Dropout2d(dropout_rate,inplace=True)
+
+
+    def forward(self,x):
+        """
+        Args:
+            x:    input 4D Pytorch tensor
+        """
+        #Compressed representation
+
+        y=self.encoder(x)
+
+        #Apply dropout in compressed encoding during training
+
+        y=self.dropout(y)
+
+        #Decode
+        y=self.decoder(y)
+
+        return y
+
+
+class NearestUsampling2D(nn.Module):
+    def __init__(self,size):
+        super(NearestUsampling2D, self).__init__()
+        self.size=size #(tuple)
+
+    def forward(self,input):
+        return F.interpolate(input, size=self.size,mode='nearest')
+
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+        self.decoder=nn.Sequential(
+            #2nd dconv layer
+            nn.BatchNorm2d(512), # [N,input_dims,1,1]
+            NearestUsampling2D((2,2)), # [N,input_dims,2,2]
+            nn.Conv2d(512,512,kernel_size=3,stride=1,padding=1), # [N,input_dims,2,2]
+            nn.BatchNorm2d(512),
+            nn.RReLU(),
+
+            #2nd dconv layer
+            NearestUsampling2D((4,4)), # [N,512,4,4]
+            nn.Conv2d(512,256,kernel_size=3,stride=1,padding=1), # [N,256,4,4]
+            nn.BatchNorm2d(256),
+            nn.RReLU(),
+
+            #3rd dconv layer
+            NearestUsampling2D((8,8)), # [N,256,8,8]
+            nn.Conv2d(256,128,kernel_size=3,stride=1,padding=1), # [N,128,8,8]
+            nn.BatchNorm2d(128),
+            nn.RReLU(),
+
+            #4th dconv layer
+            NearestUsampling2D((16,16)), # [N,128,16,16]
+            nn.Conv2d(128,64,kernel_size=3,stride=1,padding=1), # [N,64,16,16]
+            nn.BatchNorm2d(64),
+            nn.RReLU(),
+
+            #5th dconv layer
+            NearestUsampling2D((34,34)), # [N,64,34,34]
+            nn.Conv2d(64,32,kernel_size=5,stride=1,padding=1), # [N,32,32,32]
+            nn.BatchNorm2d(32),
+            nn.RReLU(),
+
+            #6th dconv layer
+            NearestUsampling2D((64,64)), # [N,32,64,64]
+            nn.Conv2d(32,16,kernel_size=5,stride=1,padding=1),# [N,16,62,62]
+            nn.BatchNorm2d(16),
+            nn.RReLU(),
+
+            #6th dconv layer
+            NearestUsampling2D((98,98)), # [N,16,98,98]
+            nn.Conv2d(16,3,kernel_size=5,stride=1,padding=1), # [N,3,96,96]
+            nn.Sigmoid())
+
+    def forward(self,x):
+         return self.decoder(x)
---
+++ autoencoder_train.py
@@ -0,0 +1,240 @@
+from __future__ import print_function
+from MyModels import AutoEncoder
+from PIL import Image
+from scipy.ndimage.interpolation import rotate
+from tensorboardX import SummaryWriter
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets, transforms
+
+import argparse
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import pytorch_ssim
+import random
+import sys
+import time
+import h5py
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.models as models
+from tqdm   import tqdm
+
+
+def reconstruction_loss(args, x, y):
+    L1_loss = torch.nn.L1Loss(reduction='elementwise_mean')
+
+    ssim_loss = pytorch_ssim.SSIM(window_size=args.window_size)  #Average
+    loss = (1-args.alpha) * L1_loss(x,y) + \
+            args.alpha * torch.clamp( (1-ssim_loss(x,y))/2,0,1)
+
+    return loss
+
+
+class TrainingDataset(Dataset):
+    """Training set with only normal images """
+
+    def __init__(self, filename, transform=None):
+        """
+        Args:
+            filename: input h5 filename
+        """
+        h5_file = h5py.File(filename, 'r')
+        self.dataset = h5_file['x']
+        self.transform = transform
+
+    def __len__(self):
+        return self.dataset.shape[0]
+
+    def __getitem__(self, idx):
+        image = self.dataset[idx]
+
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return image
+
+
+def reconstructionTest(args, model, dataloader, epoch, path):
+    """
+    Produces an image with reconstruction results 
+
+    Args:
+        args:           arguments wrapper
+        model:
+        dataloader:     pytorch dataloader
+
+    Output:
+        none    
+    """
+    model.eval()
+    with torch.no_grad():
+        for data in dataloader:
+            targets = model(data)
+            break
+
+    #Concatenate GT
+    output = torch.cat((targets, data), dim=0)
+
+    #Save
+    filename = path + "/Reconstruction_Epoch{:04d}".format(epoch)
+    torchvision.utils.save_image(
+        output.cpu(), filename, nrow=output.shape[0] // 2, padding=5)
+
+
+def save_model(args, model, epoch):
+    """
+    saves a checkpoint so that model weight can later be used for inference
+    Args:
+    model:  pytorch model
+    """
+    path = './model_' + args.name
+    if not os.path.exists(path):
+        os.mkdir(path)
+    torch.save(model.state_dict(),
+               path + '/checkpoint_epoch_{}.pt'.format(epoch))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='AutoEccoder for compressed representation')
+    parser.add_argument(
+        '--batch-size',
+        type=int,
+        default=64,
+        metavar='N',
+        help='input batch size for training (default: 64)')
+    parser.add_argument(
+        '--alpha',
+        type=float,
+        default=0.85,
+        metavar='a',
+        help='weight of SSIM in loss function (Default=0.85)')
+    parser.add_argument(
+        '--window-size',
+        type=int,
+        default=11,
+        help='Window size for SSIM loss (Default=11 pixels)')
+    parser.add_argument(
+        '--epochs',
+        type=int,
+        default=20,
+        help='number of training epochs (Default=20)')
+    parser.add_argument(
+        '--lr', type=float, default=1e-4, help='learning rate (Default=1E-4)')
+    parser.add_argument(
+        '--eval_images',
+        type=int,
+        default=5,
+        help='number of images for reconstruction test (Default=5)')
+    parser.add_argument(
+        '--save',
+        type=int,
+        default=5,
+        metavar='N',
+        help='save model every this number of epochs (Default=5)')
+    parser.add_argument(
+        '--recon_epochs',
+        type=int,
+        default=5,
+        metavar='N',
+        help='test reconstruction quality every N epochs (Default=5)')
+    parser.add_argument(
+        '--dropout',
+        type=float,
+        default=0.3,
+        metavar='N',
+        help=
+        'apply dropout to the feature vector to ensure spread of information')
+    parser.add_argument(
+        '--name',
+        type=str,
+        default='test',
+        help=
+        'name of run (default=test')
+
+    args = parser.parse_args()
+    #Print arguments
+    for arg in vars(args):
+        sys.stdout.write('{} = {} \n'.format(arg, getattr(args, arg)))
+        sys.stdout.flush()
+    sys.stdout.write('Random torch seed:{}\n'.format(torch.initial_seed()))
+    sys.stdout.flush()
+
+    #Dateset root:
+    train_rootdir = './pcamv1/camelyonpatch_level_2_split_train_normal.h5'
+
+    #Define Dataloader
+    training_transformations = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Pad(96, padding_mode='reflect'),
+        transforms.RandomRotation(30),
+        transforms.CenterCrop(96),
+        transforms.ToTensor()
+    ])
+
+    train_loader = DataLoader(
+        TrainingDataset(train_rootdir, transform=training_transformations),
+        batch_size=args.batch_size,
+        shuffle=True)
+
+    eval_loader = DataLoader(
+        TrainingDataset(train_rootdir, transform=transforms.ToTensor()),
+        batch_size=args.eval_images,
+        shuffle=True)
+
+    #Load Model
+    model = AutoEncoder(args.dropout)
+
+    #Optimiser
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, amsgrad=True)
+    logging_dir = './logs_' + args.name
+    if not os.path.exists(logging_dir):
+        os.makedirs(logging_dir)
+
+    writer = SummaryWriter(
+        logging_dir, comment='AutoEncoder for compressed representation')
+
+    sys.stdout.write('Start training\n')
+    sys.stdout.flush()
+    n_iter = 0
+
+    #Train
+    for epoch in range(1, args.epochs + 1):
+        sys.stdout.write('Starting epoch {}/{} \n '.format(epoch, args.epochs))
+        sys.stdout.flush()
+
+        for batch_idx, data in tqdm(enumerate(train_loader)):
+            model.train()
+
+            #Get output
+            target = model(data)
+
+            #Optimise
+            optimizer.zero_grad()
+
+            #Get reconstruction loss:
+            loss = reconstruction_loss(args, data, target)
+            loss.backward()
+            optimizer.step()
+
+            #Log into write
+            writer.add_scalar('Mini-Batch-loss', loss.item(), n_iter)
+
+            n_iter += 1
+
+        #Reconstruction test
+        if epoch % args.recon_epoch == 0:
+            reconstructionTest(args, model, eval_loader, epoch, logging_dir)
+
+        if epoch % args.save == 0:
+            save_model(args, model, epoch)
+
+
+if __name__ == '__main__':
+    main()
